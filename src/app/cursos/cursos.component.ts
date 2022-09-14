4de2af33b0f6e86d3530663e57590b2b55241f49
Caroline Ferraz <projetos.crrz@gmail.com>
2022-09-14 12:10:54 -0300

    Feat: Aula 8

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[];
+
+  // cursosService: CursosService
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = "https://loiane.training";
+
+    //  for(let i=0; i<this.cursos.length; i++) {
+    //   let curso = this.cursos[i];
+    // } 
+
+    this.cursos = this.cursosService.getCursos();
+
+   }
 
   ngOnInit(): void {
   }
