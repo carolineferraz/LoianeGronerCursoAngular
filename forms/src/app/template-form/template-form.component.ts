e3a94d5d10e311c560efcb705ebf136ba95affcf
Caroline Ferraz <projetos.crrz@gmail.com>
2022-09-18 19:34:54 -0300

    Feat: Aula 80

---
+++ forms/src/app/template-form/template-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: null,
+    email: null
+  }
+
+  onSubmit(form: any) {
+
+    console.log(form);
+    console.log(this.usuario);
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
